262f12e487996fd972445fe1a030abf4ae525c5e
Thayna-prog <11251104894@alunos.umc.br>
2026-02-11 10:28:26 -0300

    .

---
+++ Aula1_aulainaugural/aulainaugural/src/EntradadeDados.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EntradadeDados {
+    public static void main (String[] args) {
+        Scanner sc= new Scanner (System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextline();
+        System.out.print("Nome: " + nome);
+
+        System.out.print("Digite sua idade: ");
+        int idade = sc.nextInt();
+        System.out.println("Idade: " + idade);
+
+        System.out.print("Digite seu peso: ");
+        double peso = sc.nextDouble();
+        System.out.println("peso: " + peso);
+
+
+
+    }
+}
---
+++ Aula1_aulainaugural/aulainaugural/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+
+        int idade = 60;
+
+        double altura = 1.56;
+
+        char sexo = 'M';
+
+        String nome = "Julia";
+
+        System.out.println( "nome: " + nome);
+
+
+    }
+}
